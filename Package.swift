96a70ea033c664e18145137f707928418ee13070
Jenkins <jenkins@regula.by>
2026-09-04 03:59:08 +0300

    RC 9.8.20417

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["RFIDStage"]),
     ],
     targets: [
-        .binaryTarget(name: "RFIDStage", url: "https://pods.regulaforensics.com/Stage/RFIDStage/9.8.20398/DocumentReaderCoreStage_rfid_9.8.20398.zip", checksum: "547fd191d89b497a513f5fa228b522fe27f88daccfea9ee30e7f14c933423c01"),
+        .binaryTarget(name: "RFIDStage", url: "https://pods.regulaforensics.com/Stage/RFIDStage/9.8.20417/DocumentReaderCoreStage_rfid_9.8.20417.zip", checksum: "264e170f158ae3bc5a4643a887f209629fe237e4ea98d3dd040404558da00f86"),
     ]
 )
